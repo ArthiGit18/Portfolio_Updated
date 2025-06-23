8bccdfd05b282a1f1bd12ed061cb37259915c585
ArthiGit18 <arthiar180@gmail.com>
2025-06-23 15:59:39 +0530

    updated cv and banner changes

--- src/Pages/Banner.jsx
+++ src/Pages/Banner.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+import DownloadIcon from '@mui/icons-material/Download';
 
 const Banner = () => {
   return (
@@ -13,11 +18,42 @@ const Banner = () => {
             I design and develop responsive, user-friendly web interfaces that help businesses grow and users stay engaged.
             Let's build something amazing together!
           </p>
-          <a href="#contact" className="talk_btn">Let's Talk</a>
+          <div className='banner_actions'>
+            <a href="#contact" className="talk_btn">Let's Talk</a>
+            <a
+              href="https://drive.google.com/file/d/1Z9_BkPqZcsGszsvnXZrPoE0Sfac0B8az/preview"
+              className="talk_btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <DownloadIcon />
+              My CV
+            </a>
+          </div>
         </div>
 
         <div className='banner_img'>
           <img src="/assets/banner/2.jpg" alt="Arthi A" />
+          <div className='banner_links'>
+            <hr></hr>
+            <a href="https://www.instagram.com/its_a_rthi/"
+              target="_blank"
+              rel="noreferrer"><InstagramIcon className='inst_icon' />
+            </a>
+            <a href="https://github.com/ArthiGit18"
+              target="_blank"
+              rel="noreferrer">
+              <GitHubIcon className='git_icon' /></a>
+            <a href="https://www.linkedin.com/in/arthianbalagan/"
+              target="_blank"
+              rel="noreferrer"><LinkedInIcon className='link_icon' />
+            </a>
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=arthianbalagan180@gmail.com"
+              className="social_link gmail"
+              target="_blank">
+              <EmailIcon className='mailb_icon' />
+            </a>
+          </div>
         </div>
 
       </div>
